ac4e1497081784e43e4bb3a1764ab012b4b62344
Chris Chapman <Chapman@apextion.com>
2016-08-16 20:37:55 -0400

    Refactored Unit test order

--- test/__models_users.js
+++ test/__models_users.js
@@ -8,19 +8,6 @@ describe('User Model', () => {
   var server;
   var testUsers;
 
-  // Test for all Users
-  it('Gets All', (done) => {
-    User.all(
-      (err) => {
-        throw new Error(err);
-      },
-      (users) => {
-        this.testUsers = users;
-        expect(this.testUsers.length).to.be.above(0);
-        done();
-      }
-    );
-  });
 
   // Add a User
   it('Adds a new User', (done) => {
@@ -37,6 +24,20 @@ describe('User Model', () => {
 
         // User.name returned from model should match user.name supplied
         expect(user.name).to.be.equal(fakeUser.name);
+        done();
+      }
+    );
+  });
+
+  // Test for all Users
+  it('Gets All', (done) => {
+    User.all(
+      (err) => {
+        throw new Error(err);
+      },
+      (users) => {
+        this.testUsers = users;
+        expect(this.testUsers.length).to.be.above(0);
         done();
       }
     );
